4f48fe1253edfa289b5e9781664e948f27c9e3ed
cankush625 <cankush625@gmail.com>
2022-07-31 11:43:45 +0530

    refactor(state): Refactor state token implementation
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- src/pyoauth/__init__.py
+++ src/pyoauth/__init__.py
@@ -10,23 +10,29 @@ class Google:
     @staticmethod
     def sign_in(redirect_uri: str, private_key: str):
         state_token = generate_state_param(private_key)
-        return google_oauth.sign_in(redirect_uri, state_token)
+        return {
+            "auth_url": google_oauth.sign_in(redirect_uri, state_token),
+            "state": state_token,
+        }
 
     @staticmethod
-    def oauth_callback(request: urllib.request.Request, state: str, redirect_uri: str):
+    def oauth_callback(
+        request: urllib.request.Request, state: str, code: str, redirect_uri: str
+    ):
         """
         Returns access token and refresh token
         :param request: Request
         :param state: State token stored locally or in cookies
+        :param code: Authorization code received from OAuth provider
         :param redirect_uri: URI to redirect after processing request
         :return: dict: {"access_token": ACCESS_TOKEN, "refresh_token": REFRESH_TOKEN}
         """
 
-        if request.headers.get("state") != state:
+        if request.cookies.get("state") != state:
             raise StateTokenDoesNotMatchError(
                 {"StateToken": STATE_TOKEN_DOES_NOT_MATCH}
             )
-        return google_oauth.get_access_token(redirect_uri, request.headers.get("code"))
+        return google_oauth.get_access_token(redirect_uri, code)
 
     @staticmethod
     def get_user_info(access_token: str) -> dict:
